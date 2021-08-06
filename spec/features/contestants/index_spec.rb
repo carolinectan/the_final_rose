b374d9bd5a4787463a57a37d739eeef5b40023f1
Caroline Tan <carolinectan3@gmail.com>
2021-08-06 00:00:07 -0400

    Add test for bachelorette's contestants index page

---
+++ spec/features/contestants/index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "bachelorette's contestants show page" do
+  before :each do
+    @bachelorette1 = Bachelorette.create!(name: 'Jessica', season_number: 3, description: 'Island Paradise')
+    @bachelorette2 = Bachelorette.create!(name: 'Becca', season_number: 7, description: 'Urban Luxury')
+
+    @contestant1 = @bachelorette1.contestants.create!(name: 'Troy', age: 37, hometown: 'Austin')
+    @contestant2 = @bachelorette1.contestants.create!(name: 'David', age: 33, hometown: 'Seattle')
+    @contestant3 = @bachelorette1.contestants.create!(name: 'Gabriel', age: 40, hometown: 'New York City')
+    @contestant4 = @bachelorette2.contestants.create!(name: 'Jared', age: 27, hometown: 'Chicago')
+
+    visit "/bachelorettes/#{@bachelorette1.id}/contestants"
+  end
+
+  it "links to the bachelorette's contestants" do
+    expect(page).to have_content(@contestant1.name)
+    expect(page).to have_content(@contestant2.name)
+    expect(page).to have_content(@contestant3.name)
+    expect(page).to have_no_content(@contestant4.name)
+
+    expect(page).to have_content(@bachelorette1.name)
+    expect(page).to have_no_content(@bachelorette2.name)
+  end
+end
